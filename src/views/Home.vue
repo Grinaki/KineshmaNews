be2630efcc013b2a3bc58bf06393c0cca458b8d6
student <you@example.com>
2022-03-19 15:42:41 +0300

    добавили мени

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <newsindex />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import newsindex from '@/views/news/index.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   newsindex
   }
 }
 </script>
---
+++ src/views/news/economics.vue
@@ -0,0 +1,13 @@
+<template>
+<newsmenu />
+Экоеомика
+</template>
+<script>
+import newsmenu from '@/components/newsmenu.vue'
+export default{
+    name:'economics'
+    components:{
+        newsmenu
+    }
+}
+</script>
